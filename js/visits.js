cd05fd68fd8fa13d19ab680674b536aa5919a19b
Nicolae-Stelian <stelu26@gmail.com>
2019-07-22 18:57:54 +0200

    log visits

--- js/visits.js
+++ js/visits.js
@@ -1,7 +1,6 @@
 
 $(function() {
-    // var url = 'http://localhost:3000/log/index.php';
-    var url = 'http://159.69.29.27/stelu-log/index.php';
+    var url = '/log/index.php';
     url += "?type=nav&page=" + window.location.pathname;
 
     $.ajax({
@@ -13,7 +12,7 @@ $(function() {
 
     $('a').on('click', function() {
         var href  = $(this).attr('href');
-        var url = 'http://159.69.29.27/stelu-log/index.php';
+        var url = '/log/index.php';
         url += "?type=click&page=" + window.location.pathname;
 
         $.ajax({
